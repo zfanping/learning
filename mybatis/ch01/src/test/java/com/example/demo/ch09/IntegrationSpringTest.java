7a05f29614b0372eba2e7285f440b0546562aee4
zfanping <zfanping@gmail.com>
2018-12-26 13:58:25 +0800

    integration test pass

--- mybatis/ch01/src/test/java/com/example/demo/ch09/IntegrationSpringTest.java
+++ mybatis/ch01/src/test/java/com/example/demo/ch09/IntegrationSpringTest.java
@@ -1,6 +1,7 @@
 package com.example.demo.ch09;
 
 import java.util.List;
+import javax.annotation.Resource;
 
 import com.example.demo.ch01.mapper.CountryMapper;
 import com.example.demo.ch01.model.Country;
@@ -9,17 +10,16 @@ import com.example.demo.ch02.model.User;
 import junit.framework.TestCase;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class IntegrationSpringTest extends TestCase {
-    @Autowired
+    @Resource
     private UserMapper userMapper;
 
-    @Autowired
+    @Resource
     private CountryMapper countryMapper;
 
     @Test
